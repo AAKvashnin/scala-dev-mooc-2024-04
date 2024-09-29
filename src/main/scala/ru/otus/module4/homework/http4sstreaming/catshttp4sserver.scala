683a642c15a17ea259b2d3462a370679ff86b144
AAKvashnin <alexey.kvashnin@gmail.com>
2024-09-29 21:15:24 +0300

    http4sstreaming homework

--- src/main/scala/ru/otus/module4/homework/http4sstreaming/catshttp4sserver.scala
+++ src/main/scala/ru/otus/module4/homework/http4sstreaming/catshttp4sserver.scala
@@ -1,19 +1,25 @@
 package ru.otus.module4.homework.http4sstreaming
 
-import cats.Functor
-import cats.data.Kleisli
+
 import cats.effect.{IO, IOApp, Resource}
 import org.http4s.{Http, HttpRoutes, Status}
 import org.http4s.dsl.io._
 import org.http4s.ember.server.EmberServerBuilder
 import com.comcast.ip4s.{Host, Port}
 import cats.effect.kernel.Ref
+import io.circe._, io.circe.generic.semiauto._, io.circe.syntax._
+
+
 
 object Restfull {
 
+  case class CounterJSon(counter:Int)
+
+  implicit val fooEncoder: Encoder[CounterJSon] = deriveEncoder[CounterJSon]
+
   type Counter[F[_]] = Ref[F, Int]
   def service(counter:Counter[IO]): HttpRoutes[IO] = HttpRoutes.of{
-    case GET -> Root / "counter"  => counter.update(_ + 1).flatMap(_=>counter.get).flatMap(x=>Ok(x.toString))
+    case GET -> Root / "counter"  => counter.update(_ + 1).flatMap(_=>counter.get).flatMap(x=>Ok(CounterJSon(x).asJson.toString()))
 
   }
 
