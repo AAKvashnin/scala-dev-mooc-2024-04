d2fc713aa56a7e7e8e533bae911b308850c04c8a
AAKvashnin <alexey.kvashnin@gmail.com>
2024-10-26 21:55:52 +0300

    http4sstreaming homework

---
+++ src/test/scala/module4/homework/http4sstreaming/CounterServiceSpec.scala
@@ -0,0 +1,13 @@
+package ru.otus.module4.homework.http4sstreaming
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class CounterServiceSpec extends AnyFlatSpec {
+
+  "check status success" should "ok" in {
+     val result = for {
+
+     }
+  }
+
+}
---
+++ src/test/scala/module4/homework/http4sstreaming/SlowServiceSpec.scala
@@ -0,0 +1,7 @@
+package ru.otus.module4.homework.http4sstreaming
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class SlowServiceSpec extends AnyFlatSpec {
+
+}
